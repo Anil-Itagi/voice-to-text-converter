297041fa995505cdfa45ac037b081c3beb3983ac
ANIL ITAGI <140312263+Anil-Itagi@users.noreply.github.com>
2024-03-18 00:13:14 +0530

    Add files via upload

---
+++ speech.js
@@ -0,0 +1,36 @@
+let form = document.querySelector("form");
+let sr = window.webkitSpeechRecognition || window.SpeechRecognition;
+let spRec = new sr();
+
+
+let languageSelect = document.getElementById("languageSelect");
+
+// Function to change language
+function changeLanguage() {
+    spRec.lang = languageSelect.value;
+}
+
+// Event listener for language selection change
+languageSelect.addEventListener("change", changeLanguage);
+
+// Initial language setting
+changeLanguage();
+
+
+
+
+spRec.continuous = true;
+spRec.interimResults = true;
+//console.log(spRec);
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    spRec.start();
+})
+spRec.onresult = res => {
+    let text = Array.from(res.results).map(r => r[0]).map(txt => txt.transcript).join(" ");
+    form[0].value = text;
+    //console.log(text);
+}
+form[2].addEventListener('click', e => {
+    spRec.stop();
+})
